00e8e73bb371d7f61ff6dcbba19394b58cf3f559
Levi <gitlevi53@gmail.com>
2026-07-26 19:44:09 -0300

    porta e ip (do cliente) dinamicos

--- chatCliente/Cliente.java
+++ chatCliente/Cliente.java
@@ -7,24 +7,30 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Cliente {
-    private static final int PORTA = 5000; //porta escolhida
+    private static final int PORTA = 5000; //porta escolhida    PS: nao é mais util com a mudanca atual kkk
     public static void main(String[] args){
         Socket socket =null;
         InputStreamReader inputLeitor = null;
         OutputStreamWriter outputEscritor = null;
         BufferedReader bufferReader = null;
         BufferedWriter bufferWriter = null;
+        Scanner scan = new Scanner(System.in); //scan jogado pra junto das variaveis
 
         try{
 
-            socket = new Socket("localhost", PORTA);
+            System.out.println("Digite o IP do servidor: "); //mudei o ip pra ser configurado pelo usuario
+            String ipServidor = scan.nextLine();
+
+            System.out.println("Digite a porta do servidor: ");//mudei a porta pra ser configurada pelo usuario
+            int portaServer = Integer.parseInt(scan.nextLine());
+            
+            socket = new Socket(ipServidor, portaServer); //mudado aq tbm
+
             inputLeitor = new InputStreamReader(socket.getInputStream());
             outputEscritor = new OutputStreamWriter(socket.getOutputStream());
 
             bufferReader = new BufferedReader(inputLeitor);
             bufferWriter = new BufferedWriter(outputEscritor);
-
-            Scanner scan = new Scanner(System.in);
 
             while(true){
                 String mensagem = scan.nextLine();
@@ -32,7 +38,7 @@ public class Cliente {
                 bufferWriter.newLine(); 
                 bufferWriter.flush();
 
-                System.out.println("Servidor: "+ bufferReader.readLine());
+                System.out.println("Servidor: "+ bufferReader.readLine()); 
 
                 if(mensagem.equalsIgnoreCase("SAIR")){
                     break;
@@ -43,6 +49,7 @@ public class Cliente {
             e.printStackTrace();
         } finally {
             try{
+                if (scan != null) scan.close(); //fechei o scanner
                 if (socket != null) socket.close();
                 if (inputLeitor != null) inputLeitor.close();
                 if (outputEscritor != null) outputEscritor.close();
